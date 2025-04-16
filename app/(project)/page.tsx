259d1cd782f2052556972f64f450d1d9c151b7b1
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2025-04-15 21:50:17 -0300

    feat: aula-02

--- app/(project)/page.tsx
+++ app/(project)/page.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Landing Page</h1>
-      <Link href="/login">
-        <button>Login</button>
-      </Link>
-    </div>
-  )
+	return (
+		<div className="flex flex-col items-center justify-center h-screen">
+			<h1 className="text-4xl font-bold mb-10">Landing Page</h1>
+			<Link
+				href="/login"
+				className="border rounded-md px-2 py-1 cursor-pointer"
+			>
+				<button type="button">Login</button>
+			</Link>
+		</div>
+	);
 }
---
+++ app/hooks/useStripe.ts
@@ -0,0 +1,81 @@
+import { type Stripe, loadStripe } from '@stripe/stripe-js';
+
+import { useEffect, useState } from 'react';
+
+export function useStripe() {
+  const [stripe, setStripe] = useState<Stripe | null>(null);
+
+  useEffect(() => {
+    async function loadStripeAsync() {
+      if (!process.env.NEXT_PUBLIC_STRIPE_PUB_KEY) {
+        throw new Error(
+          'NEXT_PUBLIC_STRIPE_PUB_KEY is not defined in the environment variables'
+        );
+      }
+
+      const stripeInstance = await loadStripe(
+        process.env.NEXT_PUBLIC_STRIPE_PUB_KEY
+      );
+
+      setStripe(stripeInstance);
+    }
+
+    loadStripeAsync();
+  }, []);
+
+  async function createPaymentStripeCheckout(checkoutData: any) {
+    if (!stripe) return;
+
+    try {
+      const response = await fetch('/api/stripe/create-pay-checkout', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(checkoutData),
+      });
+
+      const data = await response.json();
+      await stripe.redirectToCheckout({ sessionId: data.sessionId });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function createSubscriptionStripeCheckout(checkoutData: any) {
+    if (!stripe) return;
+
+    try {
+      const response = await fetch('/api/stripe/create-subscription-checkout', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(checkoutData),
+      });
+
+      const data = await response.json();
+      await stripe.redirectToCheckout({ sessionId: data.id });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function handleCreateStripePortal() {
+    const response = await fetch('/api/stripe/create-portal', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+    window.location.href = data.url;
+  }
+
+  return {
+    createPaymentStripeCheckout,
+    createSubscriptionStripeCheckout,
+    handleCreateStripePortal,
+  };
+}
